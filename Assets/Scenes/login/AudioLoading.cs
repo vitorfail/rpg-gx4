69dd0de875b9748fc374871aa668b89da539450c
Nanatsunota1 <vitor_andrademanoel@hotmail.com>
2025-05-04 17:49:29 -0300

    12312

---
+++ Assets/Scenes/login/AudioLoading.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class AudioLoading : MonoBehaviour
+{
+    private AudioSource musicSource;
+    public AudioClip audioClip;
+    public bool play = false;
+    public bool onplay = false;
+    private bool iniciate = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    { 
+        iniciate = true;
+        musicSource = GetComponent<AudioSource>();
+        musicSource.clip = audioClip;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(play && !onplay){
+            onplay =true;
+            Debug.Log("Passou aqui");
+            musicSource.Pause(); // Pausa a música
+        }
+
+        if(!play && !onplay && !iniciate){
+            iniciate= true;
+             musicSource.Play();
+        }
+    }
+
+}
