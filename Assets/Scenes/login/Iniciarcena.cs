a9cdba94a99ba81f028eef2a13f0449eef3abcba
Nanatsunota1 <vitor_andrademanoel@hotmail.com>
2025-05-10 18:24:01 -0300

    123123121 v

--- Assets/Scenes/login/Iniciarcena.cs
+++ Assets/Scenes/login/Iniciarcena.cs
@@ -6,15 +6,8 @@ public class Iniciarcena : MonoBehaviour
 {
     public AudioLoading loading;
     public AudioTheme theme;
-    public GameObject objetoA; // arraste o GameObject que contém o TextMeshPro
-    public GameObject objetoB;
-    public GameObject objetoC;
-    public GameObject objetoD;
-    public GameObject objetoE;
-    public GameObject objetoF;
-    public GameObject objetoG;
-    public GameObject objetoH;
-    public GameObject objetoI;
+    public AudioLetras letras;
+    public GameObject objetoA, objetoB, objetoC, objetoD, objetoE, objetoF, objetoG, objetoH, objetoI;
 
     private RectTransform objA;
     private RectTransform objB;
@@ -24,74 +17,47 @@ public class Iniciarcena : MonoBehaviour
     private RectTransform objF;
     private RectTransform objG;
     private RectTransform objH;
-    private RectTransform objI;
-
-
-    private TextMeshPro textoA;
-    private TextMeshPro textoB;
-    private TextMeshPro textoC;    
-    private TextMeshPro textoD;
-    private TextMeshPro textoE;
-    private TextMeshPro textoF;   
-    private TextMeshPro textoG;
-    private TextMeshPro textoH;
-    private TextMeshPro textoI;    
- 
+    private RectTransform objI; 
 
     public bool iniciar;
     public bool hasiniciar =false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        textoA = objetoA.GetComponentInChildren<TextMeshPro>();
-        textoB = objetoB.GetComponentInChildren<TextMeshPro>();
-        textoC = objetoC.GetComponentInChildren<TextMeshPro>();
-        textoD = objetoD.GetComponentInChildren<TextMeshPro>();
-        textoE = objetoE.GetComponentInChildren<TextMeshPro>();
-        textoF = objetoF.GetComponentInChildren<TextMeshPro>();
-        textoG = objetoG.GetComponentInChildren<TextMeshPro>();
-        textoH = objetoH.GetComponentInChildren<TextMeshPro>();
-        textoI = objetoI.GetComponentInChildren<TextMeshPro>();
+        objA = ProcessarObjeto(objetoA);
+        objB = ProcessarObjeto(objetoB);
+        objC = ProcessarObjeto(objetoC);
+        objD = ProcessarObjeto(objetoD);
+        objE = ProcessarObjeto(objetoE);
 
-        objA =  objetoA.GetComponent<RectTransform>();
-        objB =  objetoB.GetComponent<RectTransform>();
-        objC =  objetoC.GetComponent<RectTransform>();
-        objD =  objetoD.GetComponent<RectTransform>();
-        objE =  objetoE.GetComponent<RectTransform>();
-        objF =  objetoF.GetComponent<RectTransform>();
-        objG =  objetoG.GetComponent<RectTransform>();
-        objH =  objetoH.GetComponent<RectTransform>();
-        objI =  objetoI.GetComponent<RectTransform>();
-
-        objA.sizeDelta =new Vector2(0f, 0f);
-        objB.sizeDelta =new Vector2(0f, 0f);
-        objC.sizeDelta =new Vector2(0f, 0f);
-        objD.sizeDelta =new Vector2(0f, 0f);
-        objE.sizeDelta =new Vector2(0f, 0f);
-        objF.sizeDelta =new Vector2(0f, 0f);
-        objG.sizeDelta =new Vector2(0f, 0f);
-        objH.sizeDelta =new Vector2(0f, 0f);
-        objI.sizeDelta =new Vector2(0f, 0f);
-
-        SetAlpha(textoA, 0f);
-        SetAlpha(textoB, 0f);
-        SetAlpha(textoC, 0f); // Invisível
-
-        SetAlpha(textoD, 0f);
-        SetAlpha(textoE, 0f);
-        SetAlpha(textoF, 0f); // Invisível
-
-        SetAlpha(textoG, 0f);
-        SetAlpha(textoH, 0f);
-        SetAlpha(textoI, 0f); // Invisível
-
+        RectTransform[] objs= new RectTransform[] {objA,objB,objC, objD, objE};
+        DiminuirTodos(objs);
   
     }
 
     // Update is called once per frame
+    public void DiminuirTodos(RectTransform[] objetos)
+    {
+        foreach (RectTransform objeto in objetos)
+        {
+            objeto.localScale = new Vector3(0f,0f,0f);
+        }
+    }
     void Update(){
         if(iniciar && !hasiniciar){
             hasiniciar=true;
+            Iniciar();
+        }
+    }
+    public RectTransform ProcessarObjeto(GameObject objeto)
+    {
+        var rect = objeto.GetComponent<RectTransform>();
+        if (rect == null)
+        {
+            return null;
+        }
+        else{
+            return rect;
         }
     }
     public void SetAlpha(TextMeshPro texto, float alpha)
@@ -103,9 +69,22 @@ public class Iniciarcena : MonoBehaviour
             texto.color = corAtual;
         }
     }
-    private void Iniciar(){
+    void Iniciar(){
         loading.play = true;
         theme.play = true;
+        Aumentar(objA, 7f);
+        Aumentar(objB,3f);
+        Aumentar(objC,3f);
+
+    }
+    IEnumerator FuncaoComDelay(float f)
+    {
+        yield return new WaitForSeconds(f); // Delay de 2 segundos
+    }
+    public void Aumentar(RectTransform rect, float f){
+        FuncaoComDelay(f);
+        rect.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        letras.play= true;
     }
 
 }
