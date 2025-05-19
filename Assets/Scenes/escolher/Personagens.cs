81afee47cf38f26c84b4611d4fe2027b5ac36244
Nanatsunota1 <vitor_andrademanoel@hotmail.com>
2025-05-18 21:14:53 -0300

    melhorando

--- Assets/Scenes/escolher/Personagens.cs
+++ Assets/Scenes/escolher/Personagens.cs
@@ -10,12 +10,26 @@ public class Personagens : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Classes = "Bárbaro";
+        Classes = "Barbaro";
         Sexo = "Homem";
         Raca = "Humano";
-        GameObject objetoComTag = personagens.FirstOrDefault(go => go.CompareTag(Classes+"_"+Sexo+"_"+Raca));
-    }
+        GameObject objetoComTag = personagens.FirstOrDefault(go => go.CompareTag(Classes + "_" + Sexo + "_" + Raca));
+        Transform trans = objetoComTag.GetComponent<Transform>();
+        trans.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
+    }
+    public void Mudar()
+    {
+        GameObject objetoComTag = personagens.FirstOrDefault(go => go.CompareTag(Classes + "_" + Sexo + "_" + Raca));
+        Transform trans = objetoComTag.GetComponent<Transform>();
+        trans.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
+    public void Desaparecer()
+    {
+        GameObject objetoComTag = personagens.FirstOrDefault(go => go.CompareTag(Classes + "_" + Sexo + "_" + Raca));
+        Transform trans = objetoComTag.GetComponent<Transform>();
+        trans.localScale = new Vector3(0f, 0f, 0f);
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/Scenes/escolher/img_classes/SelecionarClasses.cs
+++ Assets/Scenes/escolher/img_classes/SelecionarClasses.cs
@@ -1,23 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
 public class SelecionarClasses : MonoBehaviour
 {
     public Personagens person;
     public ButtonSoundController sound;
     public TextMeshProUGUI uiText;   // Referência ao componente de texto da UI
-    public string[] messages;  
+    public string[] messages;
+    public string[] clas;
     public Image uiImage;              // Referência para o componente Image da UI
     public Sprite[] sprites;           // Array de imagens (sprites) que serão trocadas
     private int currentIndex = 0;
     public void NextImage()
     {
+        clas = new string[] { "Barbaro","Bardo","Bruxo","Clerigo","Druida","Feiticeiro","Guerreiro","Ladino","Monge","Paladino","Mago","Ranger",};
         if (sprites.Length == 0) return;
 
         currentIndex = (currentIndex + 1) % sprites.Length;
         uiImage.sprite = sprites[currentIndex];
         uiText.text = messages[currentIndex];
-        person.Classes = messages[currentIndex];
+        person.Desaparecer();
+        person.Classes = clas[currentIndex];
+        person.Mudar();
         sound.PlayClickSound();
     }
     public void PreviousImage()
@@ -26,7 +31,9 @@ public class SelecionarClasses : MonoBehaviour
         currentIndex = (currentIndex - 1 + sprites.Length) % sprites.Length;
         uiImage.sprite = sprites[currentIndex];
         uiText.text = messages[currentIndex];
-        person.Classes = messages[currentIndex];
+        person.Desaparecer();
+        person.Classes = clas[currentIndex];
+        person.Mudar();
         sound.PlayClickSound();
     }
 }
