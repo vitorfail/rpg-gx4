240c71a826bad3c4da868abff2f5d2cc5ea77d1a
Nanatsunota1 <vitor_andrademanoel@hotmail.com>
2025-05-21 00:58:11 -0300

    add feiticeiro

--- Assets/Scenes/escolher/Personagens.cs
+++ Assets/Scenes/escolher/Personagens.cs
@@ -10,6 +10,7 @@ public class Personagens : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Diminuir_todos();
         Classes = "Barbaro";
         Sexo = "Homem";
         Raca = "Humano";
@@ -18,17 +19,31 @@ public class Personagens : MonoBehaviour
         trans.localScale = new Vector3(1.0f, 1.0f, 1.0f);
 
     }
+    public void Diminuir_todos()
+    {
+        foreach (GameObject obj in personagens)
+        {
+            Transform trans = obj.GetComponent<Transform>();
+            trans.localScale = new Vector3(0f, 0f, 0f);
+        }
+    }
     public void Mudar()
     {
         GameObject objetoComTag = personagens.FirstOrDefault(go => go.CompareTag(Classes + "_" + Sexo + "_" + Raca));
-        Transform trans = objetoComTag.GetComponent<Transform>();
-        trans.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        if (objetoComTag)
+        {
+            Transform trans = objetoComTag.GetComponent<Transform>();
+            trans.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
     }
     public void Desaparecer()
     {
         GameObject objetoComTag = personagens.FirstOrDefault(go => go.CompareTag(Classes + "_" + Sexo + "_" + Raca));
-        Transform trans = objetoComTag.GetComponent<Transform>();
-        trans.localScale = new Vector3(0f, 0f, 0f);
+        if (objetoComTag)
+        {
+            Transform trans = objetoComTag.GetComponent<Transform>();
+            trans.localScale = new Vector3(0f, 0f, 0f);
+        }
     }
     // Update is called once per frame
     void Update()
