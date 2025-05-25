4a2657efa855e222b3bea6c82f4d5a0657ff8789
Nanatsunota1 <vitor_andrademanoel@hotmail.com>
2025-05-25 00:36:58 -0300

    aasdasdas

--- Assets/Scenes/escolher/img_classes/SelecionarClasses.cs
+++ Assets/Scenes/escolher/img_classes/SelecionarClasses.cs
@@ -3,7 +3,13 @@ using UnityEngine.UI;
 using TMPro;
 using System.Collections.Generic;
 public class SelecionarClasses : MonoBehaviour
-{
+{   
+    public Atributos forca;
+    public Atributos destreza;
+    public Atributos cosntittuicao;
+    public Atributos inteligencia;
+    public Atributos sabedoria;
+    public Atributos carisma;
     public Personagens person;
     public ButtonSoundController sound;
     public TextMeshProUGUI uiText;   // Referência ao componente de texto da UI
@@ -21,6 +27,114 @@ public class SelecionarClasses : MonoBehaviour
         uiImage.sprite = sprites[currentIndex];
         uiText.text = messages[currentIndex];
         person.Desaparecer();
+        if (clas[currentIndex] == "Barbaro")
+        {
+            forca.Attr(350.00f);
+            destreza.Attr(100.00f);
+            cosntittuicao.Attr(200.00f);
+            inteligencia.Attr(30.00f);
+            sabedoria.Attr(100.00f);
+            carisma.Attr(100.00f);
+        }
+        if (clas[currentIndex] == "Bardo")
+        {
+            forca.Attr(100.00f);
+            destreza.Attr(100.00f);
+            cosntittuicao.Attr(60.00f);
+            inteligencia.Attr(60.00f);
+            sabedoria.Attr(100.00f);
+            carisma.Attr(350.00f);
+        }
+        if (clas[currentIndex] == "Bruxo")
+        {
+            forca.Attr(20.00f);
+            destreza.Attr(60.00f);
+            cosntittuicao.Attr(60.00f);
+            inteligencia.Attr(80.00f);
+            sabedoria.Attr(100.00f);
+            carisma.Attr(350.00f);
+        }
+        if (clas[currentIndex] == "Clerigo")
+        {
+            forca.Attr(60.00f);
+            destreza.Attr(30.00f);
+            cosntittuicao.Attr(100.00f);
+            inteligencia.Attr(100.00f);
+            sabedoria.Attr(350.00f);
+            carisma.Attr(100.00f);
+        }
+        if (clas[currentIndex] == "Druida")
+        {
+            forca.Attr(80.00f);
+            destreza.Attr(70.00f);
+            cosntittuicao.Attr(70.00f);
+            inteligencia.Attr(350.00f);
+            sabedoria.Attr(100.00f);
+            carisma.Attr(80.00f);
+        }
+        if (clas[currentIndex] == "Feiticeiro")
+        {
+            forca.Attr(60.00f);
+            destreza.Attr(100.00f);
+            cosntittuicao.Attr(80.00f);
+            inteligencia.Attr(100.00f);
+            sabedoria.Attr(100.00f);
+            carisma.Attr(350.00f);
+        }
+        if (clas[currentIndex] == "Guerreiro")
+        {
+            forca.Attr(350.00f);
+            destreza.Attr(100.00f);
+            cosntittuicao.Attr(80.00f);
+            inteligencia.Attr(100.00f);
+            sabedoria.Attr(100.00f);
+            carisma.Attr(100.00f);
+        }
+        if (clas[currentIndex] == "Ladino")
+        {
+            forca.Attr(100.00f);
+            destreza.Attr(350.00f);
+            cosntittuicao.Attr(80.00f);
+            inteligencia.Attr(80.00f);
+            sabedoria.Attr(70.00f);
+            carisma.Attr(70.00f);
+        }
+        if (clas[currentIndex] == "Monge")
+        {
+            forca.Attr(100.00f);
+            destreza.Attr(350.00f);
+            cosntittuicao.Attr(150.00f);
+            inteligencia.Attr(80.00f);
+            sabedoria.Attr(70.00f);
+            carisma.Attr(70.00f);
+        }
+        if (clas[currentIndex] == "Paladino")
+        {
+            forca.Attr(150.00f);
+            destreza.Attr(30.00f);
+            cosntittuicao.Attr(200.00f);
+            inteligencia.Attr(80.00f);
+            sabedoria.Attr(70.00f);
+            carisma.Attr(350.00f);
+        }
+        if (clas[currentIndex] == "Mago")
+        {
+            forca.Attr(20.00f);
+            destreza.Attr(30.00f);
+            cosntittuicao.Attr(100.00f);
+            inteligencia.Attr(350.00f);
+            sabedoria.Attr(100.00f);
+            carisma.Attr(100.00f);
+        }
+        if (clas[currentIndex] == "Ranger")
+        {
+            forca.Attr(20.00f);
+            destreza.Attr(350.00f);
+            cosntittuicao.Attr(100.00f);
+            inteligencia.Attr(100.00f);
+            sabedoria.Attr(20.00f);
+            carisma.Attr(80.00f);
+        }
         person.Classes = clas[currentIndex];
         person.Mudar();
         sound.PlayClickSound();
