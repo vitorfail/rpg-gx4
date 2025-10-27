2cec3011aa52c7461729f0782b93beffca16d5af
Nanatsunota1 <vitor_andrademanoel@hotmail.com>
2025-10-26 22:22:44 -0300

    asdasdas

--- Assets/Resources/Caracters/Personagens.cs
+++ Assets/Resources/Caracters/Personagens.cs
@@ -34,7 +34,24 @@ public class Personagens : MonoBehaviour
         {
             renderer.material = material;
         }
-        GameObject armaPrefab = Resources.Load<GameObject>($"Items/Armas/Espadas/Espada-1/espada1");
+        GameObject armaPrefab = null;
+        if (Classes == "Barbaro" || Classes == "Guerreiro" || Classes == "Paladino" || Classes == "Barbaro")
+        {
+            string[] espadas_machados_martelos = { $"Items/Armas/Espadas/Espada-1/espada1", $"Items/Armas/Espadas/Espada-1/espada1", $"Items/Armas/Espadas/Espada-1/espada1" };
+
+            // 2. Gere um índice aleatório dentro do intervalo do array
+            int index = UnityEngine.Random.Range(0, espadas_machados_martelos.Length);
+            armaPrefab = Resources.Load<GameObject>(espadas_machados_martelos[index]);
+        }
+        if (Classes=="Mago"|| Classes=="Druida" || Classes=="Bruxo"|| Classes=="Feiticeiro"|| Classes=="Clerigo")
+        {
+            string[] cajados = { $"Items/Armas/Cajados/Cajado-1/cajado1", $"Items/Armas/Cajados/Cajado-2/cajado2", $"Items/Armas/Cajados/Cajado-3/cajado3",$"Items/Armas/Cajados/Cajado-4/cajado4" };
+
+            // 2. Gere um índice aleatório dentro do intervalo do array
+            int index = UnityEngine.Random.Range(0, cajados.Length);
+
+            armaPrefab = Resources.Load<GameObject>(cajados[index]);
+        }
         if (armaPrefab != null)
         {
             // Supondo que dentro do personagem existe um "ponto de ancoragem"
@@ -43,7 +60,7 @@ public class Personagens : MonoBehaviour
             if (maoEsquerda != null)
             {
                 GameObject arma = Instantiate(armaPrefab, maoEsquerda); // já fica aninhado
-                arma.transform.localPosition = new Vector3(-0.64f, -0.7f, 0f);
+                //arma.transform.localPosition = new Vector3(-0.64f, -0.7f, 0f);
                 arma.transform.localRotation = Quaternion.Euler(0f, 0f, 180f);
 
                 SpriteRenderer sr = arma.GetComponent<SpriteRenderer>();
